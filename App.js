80da6c62472b9f9bf7486e9e0c9f8b89b1191463
subhadippaul2311 <126654387+subhadippaul2311@users.noreply.github.com>
2023-03-07 00:58:19 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,32 @@
+//import logo from './logo.svg';
+import './App.css';
+ import {H1} from './H1';
+ import {H2} from './H2';
+ import {H3} from './H3';
+ import {Button} from './Button'
+
+function App() {
+  return (
+    // <>
+    // <h1>I am heading one</h1>
+    // <h2> I am heading two</h2>
+    // <h3>I am heading three</h3>
+    // <button>Click Me !!</button>
+    
+    // </>
+    // <>
+  <>
+    <H1/>
+    <H2/>
+    <H3/>
+    <Button/>
+
+    </>
+    
+    
+    // </>
+    
+  );
+}
+
+export default App;
